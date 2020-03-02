bfacd68a64f266f0bd12075f1ca923806086eb96
Theodor Costache <teddy@gaim01dev01.fritz.box>
2020-03-02 21:54:51 +0100

    feat: tested install command

--- install.sh
+++ install.sh
@@ -6,7 +6,9 @@ echo "Running tests..."
 python3 -m pytest ilogs_test.py || exit 1
 echo "Compiling..."
 cython ilogs.py --embed || exit 1
-gcc -Os -I /usr/include/python3.7m -o kubectl-ilogs ilogs.c -lpython3.7m -lpthread -lm -lutil -ldl || exit 1
+gcc -Os -I $(find /usr/include -name python3.*m -print -quit) -o\
+        kubectl-ilogs ilogs.c -l $(find /usr/include -name python3.*m -printf "%f" -quit)\
+        -lpthread -lm -lutil -ldl || exit 1
 echo "Installing..."
-sudo mv ./kubectl-ilogs /usr/local/bin
+mv ./kubectl-ilogs /usr/local/bin
 echo "Installation completed successfully!"
